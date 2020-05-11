8fbf4e95f7d51b9dff44de17aaa606c4dacdc1e2
Quinton Fults <coding.banjo@gmail.com>
2020-05-11 13:06:50 -0700

    code

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+let nums = [1, 2, 3, 4]
+
+let [ , , , num4 ] = nums
+
+console.log(num4)
+
+let dog = {
+  name: 'Beef',
+  age: 2,
+  breed: 'chihuahua'
+}
+
+let { breed, age, name } = dog
+
+console.log(name, age, breed)
--- math.js
+++ math.js
@@ -1,3 +1,10 @@
+let [ , , op, num1, num2 ] = process.argv
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+// let op = process.argv[2]
+// let num1 = parseInt(process.argv[3])
+// let num2 = parseInt(process.argv[4])
+
 const adder = (a, b) => {
   return a + b
 }
@@ -10,8 +17,24 @@ const multiplier = (a, b) => {
 const divider = (a, b) => {
   return a / b
 }
+const modulus = (a, b) => {
+  return a % b
+}
 
-console.log(adder(5, 7))
-console.log(subtracter(5, 7))
-console.log(multiplier(5, 7))
-console.log(divider(5, 7))
+switch (op) {
+  case 'add':
+    console.log(adder(num1, num2))
+    break
+  case 'subtract':
+    console.log(subtracter(num1, num2))
+    break
+  case 'multiply':
+    console.log(multiplier(num1, num2))
+    break
+  case 'divide':
+    console.log(divider(num1, num2))
+    break
+  case 'modulus':
+    console.log(modulus(num1, num2))
+    break
+}
